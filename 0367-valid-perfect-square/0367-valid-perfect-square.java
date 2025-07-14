3104b9474536e9a51a060af23e6a4321b3abdc74
Sridhar-Developer-009 <sridharvenkatesan09@gmail.com>
2025-07-14 19:08:13 +0530

    Time: 0 ms (100%), Space: 40.1 MB (97.61%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-         double ans=Math.pow(num,0.5);
-        double a=ans*10;
-        int b= (int) (ans);
-        int c= b*10;
+         int ans=(int)Math.pow(num,0.5);
 
-        return a==c;
+
+        return ans*ans==num;
     }
 }
